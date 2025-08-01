d4235d798d7b151d618f225ae08659cfe839141a
Aditya <Aditya@ADITYA>
2025-08-01 12:04:52 +0530

    Add project files.

---
+++ Demo_to_delegates/Notifier.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_to_delegates
+{
+    //cretaing a delegate types
+    public delegate void Notify(string message);
+    public class Notifier
+    {
+        public void SendEmail(string message)
+        {
+            Console.WriteLine($"Email sent with message: {message}");
+        }
+        public void SendSMS(string message)
+        {
+            Console.WriteLine($"SMS sent with message: {message}");
+        }
+    }
+}
